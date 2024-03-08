0af01a7cada3129ab1333cb70a9f5355b157f503
feliphearaujo <feliphearaujoti@gmail.com>
2024-03-07 21:58:11 -0300

    Utilizando row e sizedbox

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,19 @@ class MainApp extends StatelessWidget { // construtor da classe mais extendendo 
 
   @override
   Widget build(BuildContext context){ //BuildContext -> retorna uma série de parametros do seu aplicativo
-   return MaterialApp(
+   return MaterialApp( // Montando o tema da sua aplicacao
     home: Scaffold(
       appBar: AppBar(
         title: const Text('Hello World'),
       ),
       body: const Padding(padding: EdgeInsets.all(16), //padding coloca borda edgeInsets coloca a borda
-      child: Text("Olá Mundo"),) 
+      child: Row(children: [
+          Text("Olá Mundo"),
+          SizedBox(width: 10,),//dando espaço
+          Text("Olá Mundo 2"),
+        ]),) 
     ),//janela principal
-   );// Montando o tema da sua aplicacao
+   );
   }
 
 }
